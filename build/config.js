1447e5953eb0b46b695cb83880657ee4c80ed0fb
zhoujunpeng <zjp0432@163.com>
2018-08-01 18:22:12 +0800

    修改publich目录名称

--- build/config.js
+++ build/config.js
@@ -1,7 +1,7 @@
 const Path = require('path');
 
 const SourceDir = Path.resolve(__dirname,'../src');
-const DistDir = Path.join(__dirname,'../dist');
+const DistDir = Path.join(__dirname,'../docs');
 const DllDir = Path.join(__dirname,'../dll');
 const RootDir = Path.join(__dirname,'../');
 
